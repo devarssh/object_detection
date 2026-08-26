2f3bd583fc1b0a1bba16119403ce00510d75a7e0
devarssh <devarshchauahn@users.noreply.github.com>
2026-08-26 15:15:00 +0530

    refactor(slim): improve quantization error tolerance in PTQ

--- ppdet/slim/quant.py
+++ ppdet/slim/quant.py
@@ -87,3 +87,4 @@ class PTQ(object):
                              input_spec=None):
         self.ptq.save_quantized_model(quant_model, quantize_model_path,
                                       input_spec)
+
